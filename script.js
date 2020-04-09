e0081605ef0f1df899d1fc3c5bad169d4fc9cea4
KoshcheevPA <42152174+KoshcheevPA@users.noreply.github.com>
2020-04-10 01:32:17 +0300

    add pikachu

---
+++ script.js
@@ -0,0 +1,37 @@
+﻿let images = document.querySelectorAll('.animal');
+let greetings = document.querySelector('.greetings'); 
+
+const pikaPath = 'pika.gif';
+const beePath = 'bee.gif';
+const greetPika = 'пика пика';
+const greetBee = 'бзззз';
+const complexGreet = 'бзз пика';
+
+let pikaButton = document.querySelector('.pikaButton')
+let beeButton = document.querySelector('.beeButton')
+let pikaBeeButton = document.querySelector('.pikaBeeButton')
+
+let setButtonListener = (node, imagesPath, greet) => {
+  node.addEventListener('click', () => {
+    for(let it of images) {
+      it.src = imagesPath;
+    }
+    greetings.textContent = 'Доброго утречка! ' + greet; 
+  }) 
+}
+
+setButtonListener(pikaButton, pikaPath, greetPika);
+setButtonListener(beeButton, beePath, greetBee);
+
+pikaBeeButton.addEventListener('click', () => {
+    images.forEach((item, index, arr) => {
+      if((index + 1) < (arr.length / 2)) {
+         item.src = 'pika.gif';
+      } else {
+         item.src = 'bee.gif';
+      }
+    })
+    greetings.textContent = 'Доброго утречка! ' + complexGreet;
+})
+
+
