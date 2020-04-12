36abd097fd3a3636430f520c298baa1342edeba2
KoshcheevPA <42152174+KoshcheevPA@users.noreply.github.com>
2020-04-13 02:14:45 +0300

    Add active button

--- script.js
+++ script.js
@@ -7,12 +7,19 @@ const greetPika = 'пика пика';
 const greetBee = 'бзззз';
 const complexGreet = 'бзз пика';
 
-let pikaButton = document.querySelector('.pikaButton')
-let beeButton = document.querySelector('.beeButton')
-let pikaBeeButton = document.querySelector('.pikaBeeButton')
+let pikaButton = document.querySelector('.pikaButton');
+let beeButton = document.querySelector('.beeButton');
+let pikaBeeButton = document.querySelector('.pikaBeeButton');
+let buttons = document.querySelectorAll('.btn');
+
+let setActiveButton = (element) => {
+  for(let el of buttons) el.classList.remove('btn-active');
+  element.classList.add('btn-active');
+}
 
 let setButtonListener = (node, imagesPath, greet) => {
   node.addEventListener('click', () => {
+    setActiveButton(node);
     for(let it of images) {
       it.src = imagesPath;
     }
@@ -24,6 +31,7 @@ setButtonListener(pikaButton, pikaPath, greetPika);
 setButtonListener(beeButton, beePath, greetBee);
 
 pikaBeeButton.addEventListener('click', () => {
+    setActiveButton(pikaBeeButton);
     images.forEach((item, index, arr) => {
       if((index + 1) < (arr.length / 2)) {
          item.src = pikaPath;
